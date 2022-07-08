817e4ebcb486fe2cb36de2fad973f2fd1a01f14e
Filip Casey <filipcasey@Filips-MacBook-Pro.local>
2022-07-08 15:24:17 -0600

    test-ready

--- src/scripts/utilities.js
+++ src/scripts/utilities.js
@@ -14,7 +14,34 @@ function spacedList(array) {
   return spaced_string.trim();
 }
 
+function missingParameterMessage(params, res, endpoint_name) {
+  var missing_params = [];
+  for (const [key, value] of Object.entries(params)) {
+    if (value == null) {
+      missing_params.push(key);
+    }
+  }
+  if (missing_params.length == Object.keys(params).length) {
+    res.status(200);
+    res.json({
+      message: `Welcome to the C2C ${endpoint_name} endpoint. For information on using this endpoint, please address the documentation.`,
+    });
+  } else {
+    res.status(400);
+    res.json({
+      errors: [
+        {
+          status: 400,
+          title: "Missing parameters",
+          detail: `Missing ${missing_params}`,
+        },
+      ],
+    });
+  }
+}
+
 module.exports = {
   yearRange: yearRange,
   spacedList: spacedList,
+  missingParameterMessage: missingParameterMessage,
 };
